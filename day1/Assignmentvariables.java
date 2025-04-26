ac7d4b6f24afaffcd941537dde670ce61006abcd
sivaramansj95 <160650528+sivaramansj95@users.noreply.github.com>
2025-04-26 23:30:59 +0530

    Add files via upload
    week1 day1 assignments are added

---
+++ day1/Assignmentvariables.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Assignmentvariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20;
+		int c =a+b;
+		char name = 's';
+		String siva = "Testleaf mark is";
+		System.out.println(name);
+		System.out.println(siva + c);
+	}
+
+}
