702c57fc7f28aba42fcf33c8d40277047740ff45
sivaramansj95 <160650528+sivaramansj95@users.noreply.github.com>
2025-04-27 12:21:42 +0530

    Update Assignmentvariables.java

--- day1/Assignmentvariables.java
+++ day1/Assignmentvariables.java
@@ -5,11 +5,14 @@ public class Assignmentvariables {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10,b=20;
+		float n = 4.444f;
 		int c =a+b;
 		char name = 's';
 		String siva = "Testleaf mark is";
 		System.out.println(name);
 		System.out.println(siva + c);
+		System.out.println(n);
+		
 	}
 
 }
