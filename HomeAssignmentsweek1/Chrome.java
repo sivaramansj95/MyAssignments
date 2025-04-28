5bd4178c3ed95b85ce0f7d5f604b598930f6f9d0
sivaramansj95 <160650528+sivaramansj95@users.noreply.github.com>
2025-04-28 22:49:58 +0530

    Add files via upload

---
+++ HomeAssignmentsweek1/Chrome.java
@@ -0,0 +1,22 @@
+package HomeAssignmentsweek1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version=91.0f;
+		String developer="Google";
+		Boolean isBeta=false;
+		int releaseYear =2008;
+		char shortcutKey='C';
+		System.out.println("value of version is :" + version);
+		System.out.println("value of developer is :" + developer);
+		System.out.println("value of isBeta is :" + isBeta);
+		System.out.println("value of releaseYear is :" + releaseYear);
+		System.out.println("value of shortcutKey is :" + shortcutKey);	
+		
+
+	}
+
+}
